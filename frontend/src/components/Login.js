476eeb631d2fdc8718f92f91c0a167b45186c0bf
Juan Gimenez <j.martin.utn@gmail.com>
2020-07-28 05:08:02 -0300

    Login Page implemented w/ IBM App Id

---
+++ frontend/src/components/Login.js
@@ -0,0 +1,70 @@
+import React from "react";
+import "antd/dist/antd.css";
+import AppID from "ibmcloud-appid-js";
+import { Layout, Card, Button, Typography } from "antd";
+import { openNotification } from "./commons/Alert";
+import styles from "./login.module.css";
+import config from "../config.json";
+import girl from "../images/girl.png";
+import { LoginOutlined } from "@ant-design/icons";
+
+const { Content } = Layout;
+const { Text, Paragraph, Title } = Typography;
+const cardBodyStyles = {
+  display: "flex",
+  flexDirection: "column",
+  alignItems: "stretch",
+  height: "100%",
+  justifyContent: "space-around",
+};
+export default function Login() {
+  const appID = React.useMemo(() => {
+    return new AppID();
+  }, []);
+
+  (async () => {
+    try {
+      await appID.init(config);
+    } catch (e) {
+      console.log(e.message);
+    }
+  })();
+
+  const loginAction = async () => {
+    try {
+      const tokens = await appID.signin();
+      // TODO: Set the username in a react context
+      localStorage.setItem("tokenId", tokens.idToken);
+    } catch (e) {
+      openNotification("Error", e.message);
+    }
+  };
+
+  return (
+    <Layout style={{ height: "100vh" }}>
+      <Content className={styles.content}>
+        <img src={girl} width="100%" alt="GirlBG"></img>
+        <Card className={styles.card} bodyStyle={cardBodyStyles}>
+          <Text strong style={{ color: "#F52200" }}>
+            Be safe around the ones you love
+          </Text>
+          <Title style={{ fontSize: "2em", margin: 0 }}>TrackVid</Title>
+          <Paragraph>
+            Con tu registro tendrás acceso a tu código QR que será tu
+            identificación para tener acceso a lugares públicos y tener mayor
+            tranquilidad al reunirte con grupos de personas.
+          </Paragraph>
+          <Button
+            type="danger"
+            shape="round"
+            icon={<LoginOutlined />}
+            size="large"
+            onClick={loginAction}
+          >
+            INGRESAR
+          </Button>
+        </Card>
+      </Content>
+    </Layout>
+  );
+}
---
+++ frontend/src/components/commons/Alert.js
@@ -0,0 +1,11 @@
+import React from "react";
+import { notification } from "antd";
+import { SmileOutlined } from "@ant-design/icons";
+
+export const openNotification = ({ title, message }) => {
+  notification.open({
+    message: title,
+    description: message,
+    icon: <SmileOutlined style={{ color: "#108ee9" }} />,
+  });
+};
