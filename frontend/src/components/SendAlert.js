4dda9733ca94b303b42071fc36f64002f0961b03
Juan Gimenez <j.martin.utn@gmail.com>
2020-07-31 21:48:55 -0300

    Date picker added

--- frontend/src/components/SendAlert.js
+++ frontend/src/components/SendAlert.js
@@ -1,12 +1,11 @@
 import React, { useState } from "react";
-import { Modal, Checkbox, Input, Typography } from "antd";
+import { Modal, Checkbox, DatePicker, Typography } from "antd";
 import { sendAlert } from "../api/api";
 
 const { Text } = Typography;
 export function SendAlertModal({ visible, triggerModal }) {
   const [confirmLoading, setConfirmLoading] = useState(false);
   const [covid, setCovid] = useState();
-  const [health, setHealth] = useState();
 
   async function handleOk() {
     setConfirmLoading(true);
@@ -15,7 +14,6 @@ export function SendAlertModal({ visible, triggerModal }) {
       message: "Be careful someone that met one of your meetings got sick",
       email: "hola@mx1.ibm.com",
       covid19: covid,
-      health: health,
       symptoms: ["fever", "cough"],
       testedPositive: [
         { kind: "Test A", testedAt: "2020-07-30T00:26:36.396Z" },
@@ -32,8 +30,8 @@ export function SendAlertModal({ visible, triggerModal }) {
   function onChangeCheckBox(e) {
     setCovid(e.target.checked);
   }
-  function onChange(e) {
-    setHealth(e.target.checked);
+  function onChange(date, dateString) {
+    console.log(date, dateString);
   }
 
   return (
@@ -47,22 +45,18 @@ export function SendAlertModal({ visible, triggerModal }) {
       <Checkbox style={{ marginBottom: 15 }} onChange={onChangeCheckBox}>
         Soy positivo en COVID-19
       </Checkbox>
-
-      <Input
+      <br />
+      <Text>Cuándo comenzaron los síntomas?</Text>
+      <DatePicker
+        style={{ display: "block", marginBottom: 10 }}
         onChange={onChange}
-        placeholder="Describe como te encuentras brevemente"
-        style={{ marginBottom: 15 }}
       />
 
       <Text disabled>
         * Al confirmar Covid-19 positivo. Las personas que estuvieron en
-        contacto con Ud dentro de los 14 días anteriores serán notificados pero
-        NO se revelará su identidad.
+        contacto con Ud dentro de los 14 días anteriores a los síntomas serán
+        notificados pero NO será revelada su identidad.
       </Text>
     </Modal>
   );
-}
-
-function onChange(e) {
-  console.log(`checked = ${e.target.checked}`);
 }
